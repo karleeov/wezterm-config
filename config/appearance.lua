f81593cf7a70d1f761eb6cfc388d2099f06c7694
karleeov <karlkingov@gmail.com>
2024-07-28 12:25:26 +0800

    added config

--- config/appearance.lua
+++ config/appearance.lua
@@ -1,57 +1,84 @@
 local wezterm = require('wezterm')
-local gpu_adapters = require('utils.gpu_adapter')
 local colors = require('colors.custom')
 
 return {
-   animation_fps = 60,
-   max_fps = 60,
-   front_end = 'WebGpu',
-   webgpu_power_preference = 'HighPerformance',
-   webgpu_preferred_adapter = gpu_adapters:pick_best(),
+  animation_fps = 60,
+  max_fps = 60,
+  front_end = 'WebGpu',
+  webgpu_power_preference = 'HighPerformance',
 
-   -- color scheme
-   colors = colors,
+  -- color scheme
+  colors = colors,
 
-   -- background
-   background = {
-      {
-         source = { File = wezterm.GLOBAL.background },
-         horizontal_align = 'Center',
-      },
-      {
-         source = { Color = colors.background },
-         height = '100%',
-         width = '100%',
-         opacity = 0.96,
-      },
-   },
+  -- background
+  background = {
+    {
+      source = { File = wezterm.GLOBAL.background },
+      horizontal_align = 'Center',
+    },
+    {
+      source = { Color = colors.background },
+      height = '100%',
+      width = '100%',
+      opacity = 0.96,
+    },
+  },
 
-   -- scrollbar
-   enable_scroll_bar = true,
+  -- scrollbar
+  enable_scroll_bar = true,
 
-   -- tab bar
-   enable_tab_bar = true,
-   hide_tab_bar_if_only_one_tab = false,
-   use_fancy_tab_bar = false,
-   tab_max_width = 25,
-   show_tab_index_in_tab_bar = false,
-   switch_to_last_active_tab_when_closing_tab = true,
+  -- tab bar
+  enable_tab_bar = true,
+  hide_tab_bar_if_only_one_tab = false,
+  use_fancy_tab_bar = false,
+  tab_max_width = 25,
+  show_tab_index_in_tab_bar = false,
+  switch_to_last_active_tab_when_closing_tab = true,
 
-   -- window
-   window_padding = {
-      left = 5,
-      right = 10,
-      top = 12,
-      bottom = 7,
-   },
-   window_close_confirmation = 'NeverPrompt',
-   window_frame = {
-      active_titlebar_bg = '#090909',
-      -- font = fonts.font,
-      -- font_size = fonts.font_size,
-   },
-   inactive_pane_hsb = {
-      saturation = 0.9,
-      brightness = 0.65,
-   },
+  -- window
+  window_padding = {
+    left = 5,
+    right = 10,
+    top = 12,
+    bottom = 7,
+  },
+  window_close_confirmation = 'NeverPrompt',
+  window_frame = {
+    active_titlebar_bg = '#090909',
+  },
+  inactive_pane_hsb = {
+    saturation = 0.9,
+    brightness = 0.65,
+  },
+
+  -- Key bindings for switching between different shells and distributions
+  keys = {
+    { action = wezterm.action.CopyTo 'Clipboard', mods = 'CTRL', key = 'C' },
+    { action = wezterm.action.PasteFrom 'Clipboard', mods = 'CTRL', key = 'V' },
+    { action = wezterm.action.DecreaseFontSize, mods = 'CTRL', key = '-' },
+    { action = wezterm.action.IncreaseFontSize, mods = 'CTRL', key = '=' },
+    { action = wezterm.action.Nop, mods = 'ALT', key = 'Enter' },
+    { action = wezterm.action.ResetFontSize, mods = 'CTRL', key = '0' },
+    { action = wezterm.action.ToggleFullScreen, key = 'F11' },
+    { action = wezterm.action_callback(function(window, pane)
+        window:perform_action(wezterm.action.SpawnCommandInNewTab {
+          args = {'wsl', '--distribution', 'Ubuntu-24.04'}
+        }, pane)
+      end), mods = 'CTRL|SHIFT', key = 'U' },
+    { action = wezterm.action_callback(function(window, pane)
+        window:perform_action(wezterm.action.SpawnCommandInNewTab {
+          args = {'wsl', '--distribution', 'Arch'}
+        }, pane)
+      end), mods = 'CTRL|SHIFT', key = 'A' },
+    { action = wezterm.action_callback(function(window, pane)
+        window:perform_action(wezterm.action.SpawnCommandInNewTab {
+          args = {'wsl', '--distribution', 'Debian'}
+        }, pane)
+      end), mods = 'CTRL|SHIFT', key = 'D' },
+    { action = wezterm.action_callback(function(window, pane)
+        window:perform_action(wezterm.action.SpawnCommandInNewTab {
+          args = {'pwsh-preview'}
+        }, pane)
+      end), mods = 'CTRL|SHIFT', key = 'P' },
+  },
 }
