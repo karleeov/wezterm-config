b805e70f17ef9ba8d1d1190a1b848a9cf0f30f95
karleeov <karlkingov@gmail.com>
2024-08-18 22:38:26 +0800

    added design

--- config/appearance.lua
+++ config/appearance.lua
@@ -9,39 +9,39 @@ return {
   front_end = 'WebGpu',
   webgpu_power_preference = 'HighPerformance',
 
-  -- color scheme
+  -- Color scheme
   color_scheme = 'Cyberpunk',
   colors = {
     foreground = neon_blue,
-    background = colors.background,
+    background = '#000000',  -- Dark background for cyberpunk aesthetic
   },
 
-  -- background
+  -- Background settings
   background = {
     {
       source = { File = wezterm.GLOBAL.background },
       horizontal_align = 'Center',
     },
     {
-      source = { Color = colors.background },
+      source = { Color = '#000000' },  -- Consistent dark background color
       height = '100%',
       width = '100%',
       opacity = 0.9,
     },
   },
 
-  -- scrollbar
-  enable_scroll_bar = false, -- Set to false for performance
+  -- Scrollbar settings
+  enable_scroll_bar = false,  -- Keep off for better performance
 
-  -- tab bar
-  enable_tab_bar = true, -- Set to false if you don't need tabs often
-  hide_tab_bar_if_only_one_tab = true, -- Only show when multiple tabs are open
+  -- Tab bar settings
+  enable_tab_bar = true,
+  hide_tab_bar_if_only_one_tab = true,
   use_fancy_tab_bar = false,
   tab_max_width = 25,
   show_tab_index_in_tab_bar = false,
   switch_to_last_active_tab_when_closing_tab = true,
 
-  -- window
+  -- Window settings
   window_padding = {
     left = 5,
     right = 10,
@@ -50,14 +50,14 @@ return {
   },
   window_close_confirmation = 'NeverPrompt',
   window_frame = {
-    active_titlebar_bg = '#090909',
+    active_titlebar_bg = '#090909',  -- Dark title bar for a sleek look
   },
   inactive_pane_hsb = {
     saturation = 0.9,
     brightness = 0.65,
   },
 
-  -- Key bindings for switching between different shells and distributions
+  -- Key bindings
   keys = {
     { action = wezterm.action.CopyTo 'Clipboard', mods = 'CTRL', key = 'C' },
     { action = wezterm.action.PasteFrom 'Clipboard', mods = 'CTRL', key = 'V' },
