1e9d580f63afb5c787a109c01f3713594dd16dc1
karleeov <karlkingov@gmail.com>
2024-09-04 20:54:57 +0800

    added design

--- config/domains.lua
+++ config/domains.lua
@@ -3,27 +3,20 @@ local wezterm = require 'wezterm'
 local wsl_domains = wezterm.default_wsl_domains()
 
 return {
-  -- ref: https://wezfurlong.org/wezterm/config/lua/SshDomain.html
-  ssh_domains = {},
-
-  -- ref: https://wezfurlong.org/wezterm/multiplexing.html#unix-domains
-  unix_domains = {},
-
-  -- ref: https://wezfurlong.org/wezterm/config/lua/WslDomain.html
   wsl_domains = {
     {
       name = 'WSL:Arch',
       distribution = 'Arch',
-      username = 'karleeov', -- Replace with your WSL2 Arch username
-      default_cwd = '/home/karleeov', -- Replace with the correct home directory
-      default_prog = { 'fish', '-l' }, -- Adjust the shell if necessary
+      username = 'karleeov',
+      default_cwd = '/home/karleeov',
+      default_prog = { 'fish', '-l' },
     },
     {
       name = 'WSL:Ubuntu22',
       distribution = 'Ubuntu-22.04',
-      username = 'karleeov', -- Replace with your WSL2 Ubuntu username
-      default_cwd = '/home/karleeov', -- Replace with the correct home directory
-      default_prog = { 'bash', '-l' }, -- Adjust the shell if necessary
+      username = 'karleeov',
+      default_cwd = '/home/karleeov',
+      default_prog = { 'bash', '-l' },
     },
   },
 
@@ -40,10 +33,6 @@ return {
     },
   },
 
-
-  font = wezterm.font("FiraCode Nerd Font Mono"),
-  font_size = 12,
-  color_scheme = 'Builtin Solarized Dark',
   keys = {
     { action = wezterm.action.CopyTo 'Clipboard', mods = 'CTRL', key = 'C' },
     { action = wezterm.action.PasteFrom 'Clipboard', mods = 'CTRL', key = 'V' },
@@ -52,16 +41,28 @@ return {
     { action = wezterm.action.Nop, mods = 'ALT', key = 'Enter' },
     { action = wezterm.action.ResetFontSize, mods = 'CTRL', key = '0' },
     { action = wezterm.action.ToggleFullScreen, key = 'F11' },
+    
+    -- New shortcut for splitting the tab horizontally
+    { action = wezterm.action.SplitHorizontal { domain = "CurrentPaneDomain" }, mods = 'CTRL', key = '-' },
+    
+    -- New shortcut for splitting the tab vertically
+    { action = wezterm.action.SplitVertical { domain = "CurrentPaneDomain" }, mods = 'CTRL', key = '|' },
+
+    -- New shortcut for opening a new tab for Arch
+    { action = wezterm.action_callback(function(window, pane)
+        window:perform_action(wezterm.action.SpawnCommandInNewTab {
+          args = {'wsl', '--distribution', 'Arch'}
+        }, pane)
+      end), mods = 'CTRL|SHIFT', key = 'A' },
+    
+    -- New shortcut for opening a new tab for Ubuntu
     { action = wezterm.action_callback(function(window, pane)
         window:perform_action(wezterm.action.SpawnCommandInNewTab {
           args = {'wsl', '--distribution', 'Ubuntu-22.04'}
         }, pane)
       end), mods = 'CTRL|SHIFT', key = 'W' },
-    { action = wezterm.action_callback(function(window, pane)
-        window:perform_action(wezterm.action.SpawnCommandInNewTab {
-          args = {'wsl', '--distribution', 'Arch'}
-        }, pane)
-      end), mods = 'CTRL|SHIFT', key = 'Q' },
+    
+    -- New shortcut for opening a new tab for PowerShell
     { action = wezterm.action_callback(function(window, pane)
         window:perform_action(wezterm.action.SpawnCommandInNewTab {
           args = {'pwsh-preview'}
