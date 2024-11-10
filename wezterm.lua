7f9f7717dcbeb22b4c17e930ae2289b82481773f
karleeov <karlkingov@gmail.com>
2024-11-10 20:58:49 +0800

    added design

--- wezterm.lua
+++ wezterm.lua
@@ -6,6 +6,7 @@ require('events.right-status').setup()
 require('events.left-status').setup()
 require('events.tab-title').setup()
 require('events.new-tab-button').setup()
+require('events.status-bar')
 
 return Config:init()
    :append(require('config.appearance'))
